45c8bae28903021cf961dfa7d8c46c3e6f2b92fc
italo71 <102165012+italo71@users.noreply.github.com>
2022-12-18 14:44:27 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Insira um numero: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine($"O numero informado foi {num}");
+        }
+    }
+}
